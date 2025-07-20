fb38dcae1469d388dd06193fd05b34c9132d2b24
Pavel Turapin <paveltrpn@mail.ru>
2025-07-20 21:54:51 +0400

    update

--- modules/io/event_scheduler.cpp
+++ modules/io/event_scheduler.cpp
@@ -3,15 +3,25 @@ module;
 #include <iostream>
 #include <thread>
 #include <uv.h>
+#include <cstdlib>
 
 export module io:event_scheduler;
 
 namespace poller::io {
 
 export struct EventScheduler final {
+    EventScheduler() {
+        loop_ = static_cast<uv_loop_t *>( malloc( sizeof( uv_loop_t ) ) );
+    }
+
+    ~EventScheduler() {
+        //
+        free( loop_ );
+    }
 
 private:
-    std::thread _thread;
+    uv_loop_t *loop_{};
+    std::thread thread_;
 };
 
 }  // namespace poller::io
