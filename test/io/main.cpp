82dade51adfc4c307734962aa8ba9a20436b871d
Pavel Turapin <paveltrpn@mail.ru>
2025-07-21 00:42:09 +0400

    pass timer and idle

--- test/io/main.cpp
+++ test/io/main.cpp
@@ -1,11 +1,17 @@
 
 #include <iostream>
 #include <print>
+#include <thread>
+#include <chrono>
 
 import io;
 
+using namespace std::chrono_literals;
+
 auto main( int argc, char** argv ) -> int {
     poller::io::EventScheduler sched;
-
+    sched.passTimer( 1 );
+    sched.run();
+    std::this_thread::sleep_for( 10s );
     return 0;
 }
