091a0afe5979261e78f05d0b56a10387a0b4dafb
Pavel Turapin <paveltrpn@mail.ru>
2025-07-28 20:25:07 +0400

    update

--- modules/io/file.cpp
+++ modules/io/file.cpp
@@ -1,14 +1,33 @@
 module;
 
 #include <print>
+#include <vector>
+#include <memory>
+#include <uv.h>
 
 export module io:file;
 import :event_scheduler;
 
 namespace poller::io {
 
+struct ReadHandle {
+    uv_fs_t open_;
+    uv_fs_t read_;
+    uv_fs_t close_;
+    uv_buf_t buf_;
+};
+
+struct WriteHandle {
+    uv_fs_t open_;
+    uv_fs_t write_;
+    uv_fs_t close_;
+    uv_buf_t buf_;
+};
+
 export struct File final : EventScheduler {
 private:
+    std::vector<std::unique_ptr<ReadHandle>> readPool_{};
+    std::vector<std::unique_ptr<WriteHandle>> writPool_{};
 };
 
 }  // namespace poller::io
--- test/io/io_file.cpp
+++ test/io/io_file.cpp
@@ -4,5 +4,10 @@
 import io;
 
 auto main( int argc, char** argv ) -> int {
+    poller::io::File sched{};
+
+    sched.run();
+    sched.sync_wait();
+
     return 0;
 }
