5cc00216033b8fbe1a98fcab7bcbbdd28899dc89
Pavel Turapin <paveltrpn@integra-s.com>
2025-08-05 11:16:21 +0400

    add sender

---
+++ test/socket_client/app/src/main/kotlin/client/Sender.kt
@@ -0,0 +1,15 @@
+package client
+
+import java.net.Socket
+
+class Sender {
+    private val s_: Socket
+
+    init {
+        s_ = Socket()
+    }
+
+    fun OpenSocket(address: String): Unit {
+        // s_.
+    }
+}
