7ae06157f38e515afd73cc08da8c3ac11be8bf31
Pavel Turapin <paveltrpn@integra-s.com>
2025-12-07 13:14:09 +0400

    rewrite ResetEvent according this project code style

--- modules/poller/reset_event.cpp
+++ modules/poller/reset_event.cpp
@@ -7,6 +7,14 @@ export module poller:reset_event;
 
 namespace poller {
 
+// Based on Lewis Baker cppcoro async_manual_reset_event example.
+// "A manual-reset event is a coroutine/thread-synchronization primitive
+// that allows one or more threads to wait until the event is signalled by
+// a thread that calls set()".
+// All comments belongs to author.
+//
+// Original code:
+// https://github.com/lewissbaker/cppcoro
 export struct ResetEvent final {
 public:
     explicit ResetEvent( bool initiallySet = false ) noexcept
@@ -22,18 +30,20 @@ public:
 
     ~ResetEvent() = default;
 
-    struct awaiter {
-        awaiter( const ResetEvent& event ) noexcept
+    struct Awaiter {
+        explicit Awaiter( const ResetEvent& event ) noexcept
             : m_event( event ) {
             /* noop*/
         }
 
-        bool await_ready() const noexcept {
+        [[nodiscard]]
+        auto await_ready() const noexcept -> bool {
             //
             return m_event.is_set();
         }
-        bool await_suspend(
-            std::coroutine_handle<> awaitingCoroutine ) noexcept {
+
+        auto await_suspend( std::coroutine_handle<> awaitingCoroutine ) noexcept
+            -> bool {
             // Special m_state value that indicates the event is in the 'set' state.
             const void* const setState = &m_event;
 
@@ -47,7 +57,7 @@ public:
                 if ( oldValue == setState ) return false;
 
                 // Update linked list to point at current head.
-                m_next = static_cast<awaiter*>( oldValue );
+                m_next = static_cast<Awaiter*>( oldValue );
 
                 // Finally, try to swap the old list head, inserting this awaiter
                 // as the new list head.
@@ -59,7 +69,7 @@ public:
             return true;
         }
 
-        void await_resume() noexcept {
+        auto await_resume() noexcept -> void {
             //
         }
 
@@ -68,20 +78,20 @@ public:
 
         const ResetEvent& m_event;
         std::coroutine_handle<> m_awaitingCoroutine;
-        awaiter* m_next;
+        Awaiter* m_next;
     };
 
-    awaiter operator co_await() const noexcept {
+    auto operator co_await() const noexcept -> Awaiter {
         //
-        return awaiter{ *this };
+        return Awaiter{ *this };
     }
 
-    bool is_set() const noexcept {
+    auto is_set() const noexcept -> bool {
         //
         return m_state.load( std::memory_order_acquire ) == this;
     }
 
-    void set() noexcept {
+    auto set() noexcept -> void {
         // Needs to be 'release' so that subsequent 'co_await' has
         // visibility of our prior writes.
         // Needs to be 'acquire' so that we have visibility of prior
@@ -91,7 +101,7 @@ public:
             // Wasn't already in 'set' state.
             // Treat old value as head of a linked-list of waiters
             // which we have now acquired and need to resume.
-            auto* waiters = static_cast<awaiter*>( oldValue );
+            auto* waiters = static_cast<Awaiter*>( oldValue );
             while ( waiters != nullptr ) {
                 // Read m_next before resuming the coroutine as resuming
                 // the coroutine will likely destroy the awaiter object.
@@ -102,7 +112,7 @@ public:
         }
     }
 
-    void reset() noexcept {
+    auto reset() noexcept -> void {
         void* oldValue = this;
         m_state.compare_exchange_strong( oldValue, nullptr,
                                          std::memory_order_acquire );
