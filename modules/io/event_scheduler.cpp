d2236b555c014c0157f8eb9b13ead2730b3031ae
Pavel Turapin <paveltrpn@mail.ru>
2026-05-27 13:45:11 +0400

    revert io, build is BROKEN

---
+++ modules/io/event_scheduler.cpp
@@ -0,0 +1,123 @@
+
+module;
+
+#include <thread>
+#include <mutex>
+#include "uv.h"
+#include <condition_variable>
+
+export module io:event_scheduler;
+
+import log;
+
+import :list;
+
+namespace tire::io {
+
+struct EventScheduler {
+    EventScheduler()
+        : loop_{ static_cast<uv_loop_t *>( std::malloc( sizeof( uv_loop_t ) ) ) } {
+        // Try to allocate main loop handle.
+
+        // We not yet in loop thread. Calling this is safe.
+        const auto ret = uv_loop_init( loop_ );
+
+        // Loop thread initialization.
+        thread_ = std::make_unique<std::thread>( [this]() -> void {
+            // Main thread must explicitly notify this
+            // thread to start event loop.
+            std::unique_lock<std::mutex> lk{ m_ };
+            cv_.wait( lk, [this]() -> bool {
+                //
+                return run_;
+            } );
+
+            // Start event loop.
+            uv_run( loop_, UV_RUN_DEFAULT );
+
+            log::info()( "uv loop closed!" );
+        } );
+    }
+
+    // Not copyable not moveable "service-like" object.
+    EventScheduler( const EventScheduler &other ) = delete;
+    EventScheduler( EventScheduler &&other ) = delete;
+    auto operator=( const EventScheduler &other ) -> EventScheduler & = delete;
+    auto operator=( EventScheduler &&other ) -> EventScheduler & = delete;
+
+    virtual ~EventScheduler() {
+        // We already left loop thread.
+        // De-initialize and free the resources associated with an event loop.
+        uv_loop_close( loop_ );
+
+        // Release loop pointer.
+        std::free( loop_ );
+    }
+
+    auto run() -> void {
+        if ( run_ ) {
+            log::warning()( "can't do run(), context already active!" );
+            return;
+        }
+
+        {
+            std::lock_guard<std::mutex> _{ m_ };
+            run_ = true;
+        }
+        cv_.notify_one();
+
+        thread_->detach();
+    }
+
+    auto syncWait() -> void {
+        if ( run_ ) {
+            log::warning()( "can't do syncWait(), context already "
+                            "active!" );
+            return;
+        }
+
+        {
+            std::lock_guard<std::mutex> _{ m_ };
+            run_ = true;
+        }
+        cv_.notify_one();
+
+        //
+        thread_->join();
+    }
+
+protected:
+    // Submit callback to event loop.
+    // That async handle contain a pointer to specific data, that will be
+    // used in callback and callback to perform action on event loop thread itself.
+    auto schedule( void *payload, std::invocable<uv_async_t *> auto cb ) -> void {
+        // Allocate uv_async_t handle. Will be deleted in close callback.
+        const auto j = static_cast<uv_async_t *>( std::malloc( sizeof( uv_async_t ) ) );
+
+        // Store payload pointer in async handle
+        j->data = payload;
+
+        // Initilaize async handle with specific payload and callback.
+        // NOTE: this is (probably) not thread safe!
+        uv_async_init( loop_, j, cb );
+
+        // Submit job to event loop.
+        // This thread safe (one and only thread safe call across
+        // all lobuv API)
+        uv_async_send( j );
+    }
+
+private:
+    // Main and only uv loop handle.
+    uv_loop_t *loop_{};
+
+    // Loop thread.
+    std::unique_ptr<std::thread> thread_;
+
+    //
+    std::condition_variable cv_;
+    std::mutex m_;
+    bool run_{ false };
+};
+
+}  // namespace tire::io
---
+++ modules/io/io_context.cpp
@@ -0,0 +1,162 @@
+
+module;
+
+#include <cstdlib>
+#include <coroutine>
+
+#include <uv.h>
+
+export module io:context;
+
+import :event_scheduler;
+import :async;
+import :list;
+
+namespace tire::io {
+
+struct TimerHandle final {
+    uv_timer_t handle_{};
+    uint64_t timeout_{};
+    uint64_t repeat_{};
+    // uv_timer_cb type
+    void ( *cb_ )( uv_timer_t* handle ){};
+};
+
+struct ReadHandle {
+    uv_fs_t open_;
+    uv_fs_t read_;
+    uv_fs_t close_;
+    uv_buf_t buf_;
+};
+
+struct WriteHandle {
+    uv_fs_t open_;
+    uv_fs_t write_;
+    uv_fs_t close_;
+    uv_buf_t buf_;
+};
+
+export template <typename T>
+struct TimeoutAwaitable;
+
+export struct IoContext final : EventScheduler {
+    template <typename T>
+    friend struct TimeoutAwaitable;
+
+public:
+    auto timeout( uint64_t timeout ) -> TimeoutAwaitable<Task<void>>;
+
+private:
+    // Start timer repeating with interval.
+    auto repeat( uint64_t repeat, void ( *cb )( uv_timer_t* ), void* payload )
+        -> void {
+        //
+        auto t = new TimerHandle{};
+
+        //
+        t->timeout_ = 0;
+        t->repeat_ = repeat;
+        t->cb_ = cb;
+
+        uv_handle_set_data( reinterpret_cast<uv_handle_t*>( &t->handle_ ),
+                            payload );
+
+        auto timerCb = []( uv_async_t* asyncHandle ) -> void {
+            auto timer = static_cast<TimerHandle*>( asyncHandle->data );
+
+            uv_timer_init( asyncHandle->loop, &timer->handle_ );
+            uv_timer_start( &timer->handle_, timer->cb_, timer->timeout_,
+                            timer->repeat_ );
+
+            // Manually close active uv_async_t handle.
+            // It exclude this handle from event loop queue.
+            uv_close( reinterpret_cast<uv_handle_t*>( asyncHandle ),
+                      []( uv_handle_t* asyncHandle ) -> void {
+                          //
+                          std::free( asyncHandle );
+                      } );
+        };
+
+        schedule( t, timerCb );
+    };
+
+    // Fire once by timeout.
+    auto timeout( uint64_t timeout, void ( *cb )( uv_timer_t* ), void* payload )
+        -> void {
+        //
+        auto t = new TimerHandle{};
+
+        //
+        t->timeout_ = timeout;
+        t->repeat_ = 0;
+        t->cb_ = cb;
+
+        uv_handle_set_data( reinterpret_cast<uv_handle_t*>( &t->handle_ ),
+                            payload );
+
+        auto timerCb = []( uv_async_t* asyncHandle ) -> void {
+            auto timer = static_cast<TimerHandle*>( asyncHandle->data );
+
+            uv_timer_init( asyncHandle->loop, &timer->handle_ );
+            uv_timer_start( &timer->handle_, timer->cb_, timer->timeout_,
+                            timer->repeat_ );
+
+            uv_close( reinterpret_cast<uv_handle_t*>( asyncHandle ),
+                      []( uv_handle_t* asyncHandle ) -> void {
+                          //
+                          std::free( asyncHandle );
+                      } );
+        };
+
+        schedule( t, timerCb );
+    };
+};
+
+template <typename T>
+struct TimeoutAwaitable final {
+    TimeoutAwaitable( IoContext& context, uint64_t timeout )
+        : context_( context )
+        , timeout_{ timeout } {};
+
+    [[nodiscard]]
+    auto await_ready() const noexcept -> bool {
+        //
+        return false;
+    }
+
+    auto await_suspend(
+        std::coroutine_handle<typename T::promise_type> handle ) noexcept {
+        // Coroutine resume callback.
+        auto cb = []( uv_timer_t* timer ) -> void {
+            auto coroHandle =
+                static_cast<std::coroutine_handle<typename T::promise_type>*>(
+                    timer->data );
+            coroHandle->resume();
+
+            uv_close( reinterpret_cast<uv_handle_t*>( timer ),
+                      []( uv_handle_t* handle ) -> void {
+                          //
+                          delete handle;
+                      } );
+        };
+
+        coroHandle_ = handle;
+
+        context_.timeout( timeout_, cb, &coroHandle_ );
+    }
+
+    auto await_resume() const noexcept -> void {
+        //
+    }
+
+    std::coroutine_handle<typename T::promise_type> coroHandle_;
+    IoContext& context_;
+    uint64_t timeout_;
+};
+
+auto IoContext::timeout( uint64_t timeout ) -> TimeoutAwaitable<Task<void>> {
+    //
+    return { *this, timeout };
+};
+
+}  // namespace tire::io
