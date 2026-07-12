a9497fc76beb215da6d1cab163679850a311f505
Pavel Turapin <paveltrpn@mail.ru>
2026-07-12 21:52:45 +0400

    add filewatch

--- modules/io/interface.cpp
+++ modules/io/interface.cpp
@@ -2,9 +2,10 @@
 module;
 
 export module io;
+export import :async;
 export import :schedulerbase;
 export import :scheduler;
 export import :payload;
 export import :fileio;
 export import :timer;
-export import :async;
+export import :filewatch;
